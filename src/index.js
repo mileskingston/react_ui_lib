8728f8833f9ebeb60beacb78686cb81eae3c683d
Miles Kingston <itmk1@chicdmac004.hq.river-island.com>
2018-07-31 17:33:28 +0100

    initial setup of react webpack and babel

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const Index = () => (
+  <div>
+    <App />
+  </div>
+);
+
+ReactDOM.render(<Index />, document.getElementById("index"));
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+      favicon: './src/favicon.ico'
+    })
+  ]
+};
